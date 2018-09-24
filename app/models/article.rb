3dda2e0e7f3d36f91712053aca5729cc477e356a
ashernamanya <asher@pesamoni.com>
2018-09-24 15:55:11 +0000

    some changes on the db migration

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article<ActiveRecord::Base 
     
-    validates :tittle, presence:true  
-    validates :description, presence:true 
+    validates :tittle, presence:true, length: {minimum:5, maximum:10}
+    validates :description, presence:true, length: {minimum:3, maximum: 15}
+    validates :reason, presence:true, length: {minimum: 5, maximum: 20}
     
 end 
---
+++ db/migrate/20180924154809_add_reason_to_articles.rb
@@ -0,0 +1,4 @@
+class AddReasonToArticles < ActiveRecord::Migration
+  def change
+  end
+end
